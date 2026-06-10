a20a40019042c1165f6b06c5fc3b9ae768340ade
Nathaniel Cunanan <cunanannathaniel04@gmail.com>
2026-06-10 13:37:43 +0800

    feat: added routes for department.

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\DepartmentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('students', StudentController::class);
+Route::resource('departments', DepartmentController::class);
